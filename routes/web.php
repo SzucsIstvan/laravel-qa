d7f47d194ae236504ce7ed2aa08d023273c1f77e
istvan.szucs <istvan@szucs.in>
2018-11-27 22:04:52 +0100

    Implement many to many polymorphic relationship and make the vote question controls working

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ Route::post("/answers/{answer}/accept", "AcceptAnswerController")->name("answers
 
 Route::post("/questions/{question}/favorites", "FavoritesController@store")->name("question.favorite");
 Route::delete("/questions/{question}/favorites", "FavoritesController@destroy")->name("question.unFavorite");
+
+Route::post("/questions/{question}/vote", "VoteQuestionController");
